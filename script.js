37119fd7185c462701f3fbb9e9c5b8c9a3d1113b
dsound-zz <demiansims@gmail.com>
2019-12-03 18:59:27 -0500

    more game logic

--- script.js
+++ script.js
@@ -8,13 +8,50 @@ const winningCombos = [
     [1,4,7],
     [2,5,6]
 ];
- 
+
 const grid = () => Array.from(document.getElementsByClassName('q'));
+const qNumId = (qEl) => Number.parseInt(qEl.id.replace('q', '')); 
+const emptyQs = () => grid().filter(qEl => qEl.innerText === '');
+const allSame = (arr) => arr.every(qEl => qEl.innerText === arr[0].innerText && qEl.innerText !== '');
 
-const clickFn = (event) => console.log(event);
+const takeTurn = (index, letter) => grid()[index].innerText = letter;
+const opponentChoice = () => qNumId(emptyQs()[Math.floor(Math.random() * emptyQs().length)]);
 
-const enableListeners = () =>  grid().forEach(qEl => qEl.addEventListener('click', clickFn()));
-const disableListeners = () => grid().forEach(qEl => qEl.removeEventListener('click', clickFn()));
+const endGame = (winningSequence) => { console.log('Game Over!', winningSequence)  };
+
+const checkForVictory = () => {
+    let victory = false;
+    winningCombos.forEach(combo => {
+        const _grid = grid();
+        const sequence = [_grid[combo[0]], _grid[combo[1]], _grid[combo[2]]];
+        if(allSame(sequence)) {
+            victory = true; 
+            endGame();
+        }
+    });
+    return victory; 
+};
+
+const opponentTurn = () => {
+    disableListeners(); 
+    setTimeout(() => {
+        takeTurn(opponentChoice(), 'O');
+        if(!checkForVictory());
+        enableListeners();
+    }, 1000);
+    debugger
+};
+
+const clickFn = (event) => {
+    takeTurn(qNumId(event.target), 'X');
+    if(!checkForVictory());
+    opponentTurn();
+};
+
+
+
+const enableListeners = () =>  grid().forEach(qEl => qEl.addEventListener('click', () => clickFn(event)));
+const disableListeners = () => grid().forEach(qEl => qEl.removeEventListener('click', () => clickFn(event)));
 
 enableListeners();
      
